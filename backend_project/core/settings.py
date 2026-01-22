3119bdf885f48a9080cfdc82d49c0c899c7c32a4
juanignacioalonso <zalbak87@gmail.com>
2026-01-22 17:33:47 -0300

    actualizacion de las carpetas

--- backend_project/core/settings.py
+++ backend_project/core/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend_project.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend_project.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
